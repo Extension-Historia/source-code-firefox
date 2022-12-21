76773ea00a6e46172320dd803537a3fed4562b5f
Lina Lerch <64536935+linalerch@users.noreply.github.com>
2022-12-21 16:42:40 +0100

    Update popup.js

--- popup.js
+++ popup.js
@@ -4,11 +4,11 @@ $(document).ready(function () {
         ordering: false,
         info: true,
   			"language": {
-    					"search": "",
-                        "searchPlaceholder": "Search...",
-                        "info": "&nbsp;&nbsp;Showing _TOTAL_ from _MAX_ entries",
-                        "infoFiltered": "",
-                        "infoEmpty": ""
+		      "search": "",
+              "searchPlaceholder": "Search...",
+              "info": "&nbsp;&nbsp;Showing _TOTAL_ from _MAX_ entries",
+              "infoFiltered": "",
+              "infoEmpty": ""
   			},
         "sDom": '<"top"<"actions">lfpi<"clear">><"clear">rt<"bottom">'
 
